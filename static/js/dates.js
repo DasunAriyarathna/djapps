726a3c7a6107b6d8ba203d14b93946ee886d5027
sri <sri@begada>
2009-11-04 08:15:12 +1100

    date utils

--- static/js/dates.js
+++ static/js/dates.js
@@ -5,7 +5,7 @@ function difference_in_days(date1, date2)
     var date1_ms = date1.getTime();
     var date2_ms = date2.getTime();
     var diff_ms = Math.abs(date1_ms - date2_ms);
-    return Math.round(difference_ms/ONE_DAY);
+    return Math.round(diff_ms/ONE_DAY);
 }
 
 function date_cmp(date1, date2)
