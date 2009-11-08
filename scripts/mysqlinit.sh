d72ebfbf80007c567520e9256908fd5d97fad710
sri <sri@begada>
2009-11-08 20:47:57 +1100

    mysqlinit takes a root password as well

--- scripts/mysqlinit.sh
+++ scripts/mysqlinit.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 dbname=$1
-mysqladmin --user=mysql --password='password' -f drop $dbname
-mysqladmin --user=mysql --password='password' -f create $dbname
-mysql --user=mysql --password='password' -e "drop user $dbname ; "
-mysql --user=mysql --password='password' -e "create user $dbname identified by '$dbname' ; "
-mysql --user=mysql --password='password' -e "grant all on $dbname.* to '$dbname'@'localhost' ; "
+rootpassword=$2
+
+mysql --user=root --password="$rootpassword" -e "drop user $dbname ; "
+mysql --user=root --password="$rootpassword" -e "create user $dbname identified by '$dbname' ; "
+mysqladmin --user=root --password="$rootpassword" -f drop $dbname
+mysqladmin --user=root --password="$rootpassword" -f create $dbname
+mysql --user=root --password="$rootpassword" -e "grant all privileges on $dbname.* to '$dbname'@'localhost' ; "
 python2.5 manage.py syncdb
