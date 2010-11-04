594d2981dbcd8ce01a9b024e8732df12d9e2171b
Sri Panyam <Sri Panyam@localhost>
2010-11-04 18:21:52 +1100

    Adding option to make sudo run postgres user's scripts to load paths etc

--- scripts/pginit.sh
+++ scripts/pginit.sh
@@ -4,15 +4,15 @@ dbname=$1
 pgdir=$2
 
 if [ "$pgdir" = "" ]; then
-    sudo -u postgres dropdb $dbname
-    sudo -u postgres createdb $dbname
-    sudo -u postgres dropuser $dbname
-    sudo -u postgres createuser $dbname -s
+    sudo -u postgres -i dropdb $dbname
+    sudo -u postgres -i createdb $dbname
+    sudo -u postgres -i dropuser $dbname
+    sudo -u postgres -i createuser $dbname -s
 else
-    sudo -u postgres $pgdir/dropdb $dbname
-    sudo -u postgres $pgdir/createdb $dbname
-    sudo -u postgres $pgdir/dropuser $dbname
-    sudo -u postgres $pgdir/createuser $dbname -s
+    sudo -u postgres -i $pgdir/dropdb $dbname
+    sudo -u postgres -i $pgdir/createdb $dbname
+    sudo -u postgres -i $pgdir/dropuser $dbname
+    sudo -u postgres -i $pgdir/createuser $dbname -s
 fi
 python manage.py syncdb
 
