8ba52b4e22aca3e770b42eebef821db0f7b04bb5
Sri Panyam <sri.panyam@gmail.com>
2010-11-06 22:32:05 +1100

    Moving scripts back to djapps folder so distutils setup scripts will pick them up. Modifying make_gae_django.sh to copy django folder to /tmp and remove .svn files first.

--- djapps/scripts/make_gae_django.sh
+++ djapps/scripts/make_gae_django.sh
@@ -3,6 +3,10 @@
 cd $1
 version=$2
 ZIPNAME=django$version.zip
+rm -Rf /tmp/django
+cp -r django /tmp
+cd /tmp
+rm -Rf `find django/ | grep .svn`
 zip -r $ZIPNAME django/__init__.py django/bin django/core \
        django/db django/dispatch django/forms \
        django/http django/middleware django/shortcuts \
@@ -13,4 +17,4 @@ zip -r $ZIPNAME django/conf -x 'django/conf/locale/*'
 zip -r $ZIPNAME django/contrib/__init__.py \
        django/contrib/formtools
 cd -
-mv $1/$ZIPNAME $1/../
+mv /tmp/$ZIPNAME ../
--- djapps/utils/gae/django.py
+++ djapps/utils/gae/django.py
@@ -1,29 +1,25 @@
-import logging, os, sys
 
 def setup_django(django_dir = "Django-1.1.2", django_zip = "django1.1.2.zip", server_software = None):
+    """
+    Sets up django for use with app engine.
+    """
+
+    import logging, os, sys
     # remove the standard version of django - django 0.96
     for k in [ k for k in sys.modules if k.lower().startswith('django') ]:
         logging.debug(" ========== Deleting Module: " + k + ", " + str(sys.modules[k]))
         del sys.modules[k]
 
-    # force sys.path to have our own directory first in 
-    # case we want to import from it
-    django_path = django_zip
-    logging.debug("Using Prod Server ===============")
-
     if not server_software:
         server_software = os.environ.get('SERVER_SOFTWARE',"")
-
-    print >> sys.stderr, "=" * 80
-    print >> sys.stderr, "Server Software: ", server_software
 
     # Set logging and use the real django folders instead of
     # django zip in dev mode
     if server_software.startswith('Dev'):
-        logging.debug("Using Dev Server ===============" + os.path.abspath(os.curdir))
-        # django_path = os.path.abspath(django_dir)
         django_path = django_dir
         logging.getLogger().setLevel(logging.DEBUG)
+    else:
+        django_path = django_zip
 
     if django_path not in sys.path:
         sys.path.insert(0, django_path)
