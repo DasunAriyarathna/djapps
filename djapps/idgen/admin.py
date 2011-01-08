a26abc5fbe83f38c1975c12cc339630babc14a3c
Sri Panyam <sri.panyam@gmail.com>
2011-01-09 03:58:59 +1100

    Updating admin display for idgen models.

--- djapps/idgen/admin.py
+++ djapps/idgen/admin.py
@@ -2,8 +2,11 @@
 from models import IDGenerator, GeneratedID, IDGeneratorRandom, IDGeneratorLFSR
 from django.contrib import admin as djangoadmin
 
+class IDGeneratorAdmin(djangoadmin.ModelAdmin):
+    list_display = ("name","gen_type","allowed_chars","key_length")
+
 try:
-    djangoadmin.site.register(IDGenerator)
+    djangoadmin.site.register(IDGenerator, IDGeneratorAdmin)
     djangoadmin.site.register(IDGeneratorRandom)
     djangoadmin.site.register(IDGeneratorLFSR)
     djangoadmin.site.register(GeneratedID)
