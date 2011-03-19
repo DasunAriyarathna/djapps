1326ddcec09750aec87e3f076cef52121b9bbdea
Sri Panyam <sri.panyam@gmail.com>
2011-03-19 14:19:22 +0000

    Removing bobs.

--- djapps/dynamo/djmodels.py
+++ djapps/dynamo/djmodels.py
@@ -20,38 +20,3 @@ class DJCounter(models.Model):
     #
     class Admin: save_on_top = True
 
-# 
-# Big OBject fragment.
-# Idea is objects can have "extension" data over one or more of these
-# fragments.  The objects will be responsible for giving the names of these
-# objects.  Kind of act as poor man's inodes.
-#
-class DJBOBFragment(models.Model):
-    MAX_BOB_SIZE            = 512
-
-    # 
-    # Name of the bob this fragment belongs to
-    #
-    bob_name    = models.CharField(max_length = 128)
-
-    # 
-    # BOB Fragment ID
-    #
-    fragment    = models.IntegerField()
-
-    # 
-    # Contents of the BOB fragment
-    #
-    contents    = models.CharField(max_length = MAX_BOB_SIZE, default = "")
-
-    # 
-    # bob_id/bob_fragment are a unique primary key
-    #
-    class Meta:
-        unique_together = ("bob_name", "fragment")
-
-    # 
-    # Admin Interface
-    #
-    class Admin: save_on_top = True
-
