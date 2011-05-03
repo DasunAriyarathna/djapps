64af2898f82a9b5acce1f5a34d4c30e17b1e3f2f
Sri Panyam <sri.panyam@gmail.com>
2011-05-03 22:03:45 +1000

    Added extra function parameter to get_xxxvar methods to do additional convertion on the method that was extracted from request GET or POST param.

--- djapps/utils/request.py
+++ djapps/utils/request.py
@@ -1,22 +1,35 @@
 
 from djapps.utils import urls         as djurls
 
-def get_var(request, variable, default_val = ""):
+def get_getvar(request, variable, default = "", converter = None):
     if request.GET and variable in request.GET:
-        return request.GET[variable]
-    elif request.POST and variable in request.POST:
-        return request.POST[variable]
-    else:
-        return default_val
-
-def get_getvar(request, variable, default_val = ""):
-    if request.GET and variable in request.GET:
-        return request.GET[variable]
-    return default_val
+        if converter:
+            return converter(request.GET[variable])
+        else:
+            return request.GET[variable]
+    return default
 
 
-def get_postvar(request, variable, default_val = ""):
+def get_postvar(request, variable, default = "", converter = None):
     if request.POST and variable in request.POST:
-        return request.POST[variable]
-    return default_val
+        if converter:
+            return converter(request.POST[variable])
+        else:
+            return request.POST[variable]
+    return default
+
+
+def get_var(request, variable, default = "", converter = None):
+    if request.GET and variable in request.GET:
+        if converter:
+            return converter(request.GET[variable])
+        else:
+            return request.GET[variable]
+    elif request.POST and variable in request.POST:
+        if converter:
+            return converter(request.POST[variable])
+        else:
+            return request.POST[variable]
+    else:
+        return default
 
