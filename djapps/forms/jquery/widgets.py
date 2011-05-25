c25dbcc573ddaeaea3633f613e32ef7ec9a1d69d
Sri Panyam <sri.panyam@gmail.com>
2011-05-26 05:15:58 +1000

    Making date format parsing more flexible

--- djapps/forms/jquery/widgets.py
+++ djapps/forms/jquery/widgets.py
@@ -5,6 +5,7 @@ jQuery based widgets
 import time
 import datetime
 import re
+import itertools
 
 from django.forms.widgets import Widget, Select
 from django.utils import datetime_safe
@@ -68,8 +69,16 @@ class JQDateTimeWidget(widgets.SelectDateTimeWidget):
         """
         This parses the actual date from the data dictionary.
         """
-        print "Date_field, Date: ", self.date_field, data.get(self.date_field % name)
-        input_format = get_format('DATE_INPUT_FORMATS')[0]
-        date_str = data.get(self.date_field % name)
-        date = datetime.datetime.strptime(date_str, input_format)
+        print "Date_field, Date: ", self.date_field % name, data.get(self.date_field % name)
+        date = data.get(self.date_field % name)
+        if type(date) != datetime.datetime:
+            input_formats = itertools.chain(get_format('DATE_INPUT_FORMATS'), get_format('DATETIME_INPUT_FORMATS'))
+            for input_format in input_formats:
+                # try all formats
+                try:
+                    date = datetime.datetime.strptime(date, input_format)
+                    break
+                except ValueError, ve:
+                    pass
         return date.year, date.month, date.day
+
