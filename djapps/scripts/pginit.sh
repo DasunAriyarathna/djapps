e2fb06e3de1bbb84a439b94221a4455590b6ba8b
Sri Panyam <sri.panyam@gmail.com>
2011-06-12 22:10:35 +1000

    -w option to not ask for a password

--- djapps/scripts/pginit.sh
+++ djapps/scripts/pginit.sh
@@ -4,15 +4,15 @@ dbname=$1
 pgdir=$2
 
 if [ "$pgdir" = "" ]; then
-    sudo -u postgres -i dropdb $dbname
-    sudo -u postgres -i createdb $dbname
-    sudo -u postgres -i dropuser $dbname
-    sudo -u postgres -i createuser $dbname -s
+    sudo -u postgres -i dropdb -w $dbname
+    sudo -u postgres -i createdb -w $dbname
+    sudo -u postgres -i dropuser -w $dbname
+    sudo -u postgres -i createuser -w $dbname -s
 else
-    sudo -u postgres -i $pgdir/dropdb $dbname
-    sudo -u postgres -i $pgdir/createdb $dbname
-    sudo -u postgres -i $pgdir/dropuser $dbname
-    sudo -u postgres -i $pgdir/createuser $dbname -s
+    sudo -u postgres -i $pgdir/dropdb -w  $dbname
+    sudo -u postgres -i $pgdir/createdb -w $dbname
+    sudo -u postgres -i $pgdir/dropuser -w $dbname
+    sudo -u postgres -i $pgdir/createuser -w $dbname -s
 fi
 echo "SERVER SOFTWARE HERE: $SERVER_SOFTWARE"
 # python manage.py syncdb
