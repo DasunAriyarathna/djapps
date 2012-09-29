30264293a3ca79987b67a9975dc9801450164ba4
Sri Panyam <sri.panyam@gmail.com>
2012-09-29 23:51:09 +1000

    Fixing missing import

--- djapps/utils/http.py
+++ djapps/utils/http.py
@@ -1,13 +1,14 @@
 
 import sys
+from django.shortcuts import render_to_response
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 import json as djjson
 from . import api_result, codes
 
 JSON_CONTENT_TYPE = "application/json"
 
-def APIResponse(request, result, mimetype=None, status=None,
-                content_type=None, template_name = None, redirect_to = None,
+def APIResponse(request, code, result, template_name = None, redirect_to = None,
+                mimetype=None, status=None, content_type=None, 
                 format_param = "format", formatter_params = None):
     if isinstance(result, HttpResponse):
         response = result
@@ -19,18 +20,17 @@ def APIResponse(request, result, mimetype=None, status=None,
         if format == "json":
             formatter_params = formatter_params or {}
             content_type = content_type or JSON_CONTENT_TYPE
+            if code is not None: result = {'code': 0, 'value': result}
             if type(formatter_params) is list:
-                response = HttpResponse(djjson.json_encode(result, *formatter_params),
-                                        content_type = content_type)
+                json_str = djjson.json_encode(result, *formatter_params)
             else:
-                response = HttpResponse(djjson.json_encode(result, **formatter_params),
-                                        content_type = content_type)
+                json_str = djjson.json_encode(result, **formatter_params)
+            response = HttpResponse(json_str, content_type = content_type, mimetype = mimetype, status = status)
         else:
             from django.template import RequestContext
-            response_data = result[0]
             context = RequestContext(request)
-            response = render_to_response(result[1], response_data, context)
-    response["Access-Control-Allow-Origin"] = "*"
+            response = render_to_response(template_name, result, context, mimetype = mimetype)
     if content_type: response["Content-Type"] = content_type
+    response["Access-Control-Allow-Origin"] = "*"
     return response
 
