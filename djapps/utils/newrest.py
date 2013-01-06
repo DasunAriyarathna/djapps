67bef051403b1db89b2c4ca82b43a06ef048c67d
Sri Panyam <sri.panyam@gmail.com>
2013-01-06 14:24:09 +1100

    Refactoring DefaultRestHandler and making sure that RestUrl returns a method that has its own doc string so it can get picked up by sphinx

--- djapps/utils/newrest.py
+++ djapps/utils/newrest.py
@@ -35,27 +35,14 @@ from    .       import      api_result
 def manual_resource_handler(request, handler_class, handler_suffix,
                             method_param = "__method__", *args, **kwargs):
     format = djrequest.get_var(request, settings.FORMAT_PARAM)
-    method = request.method.lower()
+    method = request.method
     if method_param in request.GET:
         # Override/simulate methods not supported in the browser by
         # specifying as a get param
-        method = request.GET[method_param].lower()
+        method = request.GET[method_param]
 
-    methodname = method + "_handler_" + handler_suffix
-
-    themethod = getattr(handler_class, methodname, None)
-
-    if themethod:
-        return themethod(request, format, *args, **kwargs)
-    else:
-        # fall back to default_handler_....
-        methodname  = "default_handler_" + handler_suffix
-        themethod   = getattr(handler_class, methodname, None)
-        if themethod:
-            return themethod(request, format, *args, **kwargs)
-        else:
-            from django.http import Http404
-            raise Http404
+    themethod = handler_class.handler_for_method(method, handler_suffix)
+    return themethod(request, format, *args, **kwargs)
 
 def resturl(regex, handler_class, suffix,
             kwargs = None, name = None, prefix='',
@@ -70,6 +57,30 @@ class DefaultRestHandler(object):
     An OO style accessor for the resturl
     """
     @classmethod
+    def handler_for_method(handler_class, method, handler_suffix, use_default = True, raise_on_no_method = True):
+        method = method.strip().lower()
+        methodname = method + "_handler_" + handler_suffix
+        themethod = getattr(handler_class, methodname, None)
+        if use_default and not themethod:
+            # fall back to default_handler_....
+            methodname  = "default_handler_" + handler_suffix
+            themethod   = getattr(handler_class, methodname, None)
+        if raise_on_no_method and not themethod:
+            from django.http import Http404
+            raise Http404
+        return themethod
+
+    @classmethod
     def RestUrl(cls, regex, suffix, kwargs = None, name = None, prefix=''):
-        return resturl(regex, cls, suffix, kwargs, name, prefix, handler_function = manual_resource_handler)
+        get_method = cls.handler_for_method("get", suffix, False, False)
+        post_method = cls.handler_for_method("post", suffix, False, False)
+        default_method = cls.handler_for_method("default", suffix, False, False)
+        def real_handler(request, handler_class, handler_suffix,
+                         method_param = "__method__", *args, **kwargs):
+            return manual_resource_handler(request, handler_class, handler_suffix,
+                                           method_param, *args, **kwargs)
+        real_handler.__getdoc__ = get_method and get_method.__doc__ or ""
+        real_handler.__postdoc__ = post_method and post_method.__doc__ or ""
+        real_handler.__doc__ = default_method and default_method.__doc__ or ""
+        return resturl(regex, cls, suffix, kwargs, name, prefix, handler_function = real_handler)
 
