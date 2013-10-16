6950fb0a9a6c4ca70b9270bcd7170453047dfd81
tobias.fielitz <tobias.fielitz@localhost>
2013-10-16 00:27:04 +0000

    switched to new user model

--- djapps/auth/local/djmodels.py
+++ djapps/auth/local/djmodels.py
@@ -1,13 +1,12 @@
-from django import db
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 # 
 # A table for holding basic registration info about a user,
 # the actual user profile will be in a different class
 #
 class LocalUserRegistration(models.Model):
-    user            = models.ForeignKey(User)
+    user            = models.ForeignKey(settings.AUTH_USER_MODEL)
     activation_key  = models.CharField(max_length=128)
     salt            = models.CharField(max_length=128)
     key_expires     = models.DateTimeField()
